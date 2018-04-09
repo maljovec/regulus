9d89ee63715e5a89fdd4aed7af3d0ae925e9d65e
Dan Maljovec <maljovec002@gmail.com>
2018-04-09 05:49:05 -0600

    Adding ability to load and save prediction models and improving overall interface.

--- server/Predictor.py
+++ server/Predictor.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from sklearn import preprocessing
 from sklearn import model_selection
+from sklearn.pipeline import make_pipeline
 
 from sklearn import ensemble
 from sklearn import gaussian_process
@@ -10,6 +11,8 @@ from sklearn import neighbors
 from sklearn import neural_network
 from sklearn import svm
 from sklearn import tree
+
+import pickle
 
 # For the PNNL Data:
 # Gradient Boosting: 0.806777 +/- 0.096609
@@ -26,8 +29,21 @@ from sklearn import tree
 # KNN: 0.285822 +/- 0.503111
 # Multi-layer Perceptron: -0.241078 +/- 0.226819
 
+
 class Predictor(object):
-    def __init__(self, X, y):
+    """ A predictor class that will try to build several types of models
+        to fit a dataset. Each type of model will be cross-validated to
+        attempt to fit the hyperparameters for the model. The best model
+        (highest cv score) is retained for further prediction.
+    """
+
+    def fit(self, X, y):
+        """ Function for selecting the best model to use on the given
+            data. This method will try a bunch of different predictors
+            from scikit-learn with various parameter settings and settle
+            on the best for the given problem dataset
+            @ In, X, a matrix of
+        """
         # String name for plotting, Instance, Dictionary of parameters
         # to cross-validate
         testModels = [
@@ -83,19 +99,19 @@ class Predictor(object):
                                           'adaptive']}),
                       ('SVR', svm.SVR(),
                        {'kernel': ['sigmoid', 'rbf', 'linear', 'poly'],
-                        'degree': [2, 3, 4, 5, 6, 7],
+                        'degree': [2, 3],
                         'C': [1, 1e3, 1e6, 1e9],
-                        'gamma': [1e-2, 1e-1, 1, 10, 'auto'],
+                        'gamma': ['auto'],
                         'cache_size': [8000]}),
                       ('DT', tree.DecisionTreeRegressor(), {}),
                       ('ET', tree.ExtraTreeRegressor(), {})]
 
-        testModels = [('Gradient Boosting',
-                       ensemble.GradientBoostingRegressor(),
-                       {'loss': ['ls', 'lad', 'huber', 'quantile'],
-                        'learning_rate': [0.01, 0.05, 0.1, 0.5],
-                        'n_estimators': [25, 50, 75, 100, 250],
-                        })]
+        # testModels = [('Gradient Boosting',
+        #                ensemble.GradientBoostingRegressor(),
+        #                {'loss': ['ls', 'lad', 'huber', 'quantile'],
+        #                 'learning_rate': [0.01, 0.05, 0.1, 0.5],
+        #                 'n_estimators': [25, 50, 75, 100, 250],
+        #                 })]
 
         # Regression scoring techniques:
         # scoring = 'explained_variance'
@@ -105,17 +121,17 @@ class Predictor(object):
         # scoring = 'neg_median_absolute_error'
         scoring = 'r2'
 
-        # Z-score standardization for the inputs
-        X = preprocessing.scale(X, axis=0, with_mean=True, with_std=True)
-
         seed = 8
 
-        self.best_model = None
-        self.best_cv_score = -1
+        best_model = None
+        best_cv_score = -1
+
+        X_norm = preprocessing.scale(X)
 
         for name, model, params in testModels:
             print('Parameter searching for ' + name)
             # Cross-validate the data
+
             cvModel = model_selection.GridSearchCV(model, params,
                                                    scoring=scoring,
                                                    fit_params=None, n_jobs=1,
@@ -124,21 +140,35 @@ class Predictor(object):
                                                    pre_dispatch='2*n_jobs',
                                                    error_score='raise',
                                                    return_train_score='warn')
-            cvModel.fit(X, y)
+            cvModel.fit(X_norm, y)
             best_fit = cvModel.best_params_
+            bestimator = cvModel.best_estimator_
+
             print('Done')
             print(best_fit)
             print('Cross-validation score for best ' + name)
 
             kfold = model_selection.KFold(n_splits=10, random_state=seed)
-            cv_results = model_selection.cross_val_score(cvModel.best_estimator_, X, y, cv=kfold,
+            cv_results = model_selection.cross_val_score(bestimator,
+                                                         X_norm, y,
+                                                         cv=kfold,
                                                          scoring=scoring)
             print('Done')
             msg = "%s: %f +/- %f" % (name, cv_results.mean(), cv_results.std())
             print(msg)
-            if cv_results.mean() > self.best_cv_score:
-                self.best_cv_score = cv_results.mean()
-                self.best_model = cvModel.best_estimator_
+            if cv_results.mean() > best_cv_score:
+                best_cv_score = cv_results.mean()
+                best_model = bestimator
+
+            self.pipeline = make_pipeline(preprocessing.StandardScaler(),
+                                          best_model)
+            self.pipeline.fit(X, y)
 
     def predict(self, X):
-        return self.best_model.predict(X)
+        return self.pipeline.predict(X)
+
+    def save(self, filename='model.pk'):
+        pickle.dumps(self.pipeline, filename)
+
+    def load(self, filename='model.pk'):
+        self.pipeline = pickle.loads(filename)
